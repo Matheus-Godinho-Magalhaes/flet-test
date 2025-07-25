a326b14b0a9f204019fbac1a5ab7f5b451823333
Matheus Godinho <matheusgodinhomagalhaes@gmail.com>
2025-07-25 17:20:26 -0300

    Add exclamation point

--- aula2/1.py
+++ aula2/1.py
@@ -5,7 +5,7 @@ def main(page: ft.Page):
 
     # Primeiro eu crio as variáveis, nesse caso eu criei a variável texto,
     # depois eu defino o que essa variável vai receber
-    texto = ft.Text(value="Bem vindo ao app Godinho World!")
+    texto = ft.Text(value="!!Bem vindo ao app Godinho World!")
 
     # Depois eu adiciono essa minha variável na página
     page.add(texto)
